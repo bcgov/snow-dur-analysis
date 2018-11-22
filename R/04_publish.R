00ac27dd8b74fbd75bb9fe51ee057cf199103501
Jane Wang <yuexian.wang@gov.bc.ca>
2018-11-22 12:18:25 -0800

    Add final plots

--- R/04_publish.R
+++ R/04_publish.R
@@ -25,7 +25,7 @@ dfo_plot1 <- dfo_ts %>%
   labs(x = "", y = "Monthly ONI") +
   geom_hline(yintercept = 0) +
   scale_fill_distiller(palette = "Spectral") +
-  scale_x_date(date_breaks = "1 year", date_labels = "%Y") +
+  scale_x_date(date_breaks = "2 year", date_labels = "%Y") +
   theme_bw(base_size = 25, base_family = "Calibri") +
   theme(legend.position = "none", aspect.ratio = 0.3,
         panel.grid.major = element_line(linetype = 2, size = .4, color = "dark grey"),
@@ -53,7 +53,7 @@ dfo_plot2 <- dfo_ts2 %>%
   labs(x = "", y = "Seasonal ONI") +
   geom_hline(yintercept = 0) +
   scale_fill_distiller(palette = "Spectral") +
-  scale_x_date(date_breaks = "1 year", date_labels = "%Y") +
+  scale_x_date(date_breaks = "2 year", date_labels = "%Y") +
   theme_bw(base_size = 25) +
   theme(legend.position = "none", aspect.ratio = 0.3,
         panel.grid.major = element_line(linetype = 2, size = .4, color = "dark grey"),
@@ -64,17 +64,18 @@ dfo_plot <- dfo_plot1 + dfo_plot2 + plot_layout(ncol = 1)
 dfo_plot
 
 ## figure 5 snow cover over time
-df_cal_long %>%
+reg_plot <- df_cal_long %>%
   mutate(year = as.numeric(year)) %>%
   ggplot(aes(year, doy)) + # raw doy data unprocessed
   geom_point() +
-  geom_smooth(method = "lm") +
+  geom_smooth(method = "lm", colour = "#2b8cbe") +
   labs(x = "", y = "") + # showing doy for start and end but duration for INT and SCI
   facet_wrap(.~variable) +
-  theme_bw(base_size = 20) +
+  theme_bw(base_size = 25) +
   theme(legend.position = "none", strip.background = element_rect(fill = "transparent", colour = "grey"),
         panel.grid.major = element_line(linetype = 2, size = .4, color = "dark grey"),
         panel.grid.minor = element_line(linetype = 2, size = .4, color = "dark grey"))
+reg_plot
 
 ## figure 6 ONI correlation map
 SCI_map <- df_oni_prov %>%
@@ -87,8 +88,8 @@ SCI_map <- df_oni_prov %>%
   facet_wrap("season") +
   theme_void() +
   theme(legend.key.size = unit(0.5, "cm"), plot.title = element_text(hjust = 0.5),
-        panel.grid = element_line(colour = "transparent"), text = element_text(size = 12),
-        strip.text = element_text(size = 12), legend.text = element_text(size = 12))
+        panel.grid = element_line(colour = "transparent"), text = element_text(size = 20),
+        strip.text = element_text(size = 20), legend.text = element_text(size = 20))
 
 INT_map <- df_oni_prov %>%
   filter(measurements == "INT") %>%
@@ -100,8 +101,8 @@ INT_map <- df_oni_prov %>%
   facet_wrap("season") +
   theme_void() +
   theme(legend.key.size = unit(0.5, "cm"), plot.title = element_text(hjust = 0.5),
-        panel.grid = element_line(colour = "transparent"), text = element_text(size = 12),
-        strip.text = element_text(size = 12), legend.text = element_text(size = 12))
+        panel.grid = element_line(colour = "transparent"), text = element_text(size = 20),
+        strip.text = element_text(size = 20), legend.text = element_text(size = 20))
 
 INTs_map <- df_oni_prov %>%
   filter(measurements == "INTs") %>%
@@ -113,8 +114,8 @@ INTs_map <- df_oni_prov %>%
   facet_wrap("season") +
   theme_void() +
   theme(legend.key.size = unit(0.5, "cm"), plot.title = element_text(hjust = 0.5),
-        panel.grid = element_line(colour = "transparent"), text = element_text(size = 12),
-        strip.text = element_text(size = 12), legend.text = element_text(size = 12))
+        panel.grid = element_line(colour = "transparent"), text = element_text(size = 20),
+        strip.text = element_text(size = 20), legend.text = element_text(size = 20))
 
 INTe_map <- df_oni_prov %>%
   filter(measurements == "INTe") %>%
@@ -126,37 +127,66 @@ INTe_map <- df_oni_prov %>%
   facet_wrap("season") +
   theme_void() +
   theme(legend.key.size = unit(0.5, "cm"), plot.title = element_text(hjust = 0.5),
-        panel.grid = element_line(colour = "transparent"), text = element_text(size = 12),
-        strip.text = element_text(size = 12), legend.text = element_text(size = 12))
+        panel.grid = element_line(colour = "transparent"), text = element_text(size = 20),
+        strip.text = element_text(size = 20), legend.text = element_text(size = 20))
 
 ONI_map <- SCI_map + INT_map + INTs_map + INTe_map
 ONI_map
 
 ## figure 7 topo effects - snow measurements against elevation
-## by ECOPROVINCE
-df_full %>%
-  select(ECOPROVINCE_CODE, z, grep("SCI_20", colnames(df_full)), grep("INT_20", colnames(df_full)),
-         grep("INTs_20", colnames(df_full)), grep("INTe_20", colnames(df_full))) %>%
-  gather(key = "measurements", value = "value", -ECOPROVINCE_CODE, -z) %>%
-  mutate(year = sub(".*_", "", measurements),
-         measurements = sub("_.*", "", measurements),
-         season = case_when(month == "Dec" | month == "Jan" | month == "Feb" ~ "Winter",
-                            month == "Mar" | month == "Apr" | month == "May" ~ "Spring",
-                            month == "Jun" | month == "Jul" | month == "Aug" ~ "Summer",
-                            month == "Sep" | month == "Oct" | month == "Nov" ~ "Fall")) %>%
-  ggplot(aes(value, z, colour = ECOPROVINCE_CODE)) +
-  geom_point() +
-  facet_wrap("measurements")
+## should not keep only unique records of correlation, even though every elevation has
+## the same correlation?
+topo_plot <- df_oni %>%
+  ggplot(aes(cor, z, colour = season)) +
+  geom_smooth(method = "lm") +
+  labs(x = "Correlation", y = "Elevation (m)") +
+  scale_color_brewer(palette = "Set1", name = "") +
+  facet_wrap("measurements") +
+  theme_bw() +
+  theme(legend.key.size = unit(0.5, "cm"), plot.title = element_text(hjust = 0.5),
+        strip.background = element_rect(fill = "transparent", colour = "grey"),
+        panel.grid = element_line(colour = "transparent"), text = element_text(size = 20),
+        strip.text = element_text(size = 20), legend.text = element_text(size = 20))
+topo_plot
 
-## by season
-df_cal_long %>%
-  left_join(df_full) %>%
-  select(ECOPROVINCE_CODE, variable, doy, z, date) %>%
-  mutate(month = month(date),
-         season = case_when(month == 12 | month == 1 | month == 2 ~ "Winter",
-                            month == 3 | month == 4 | month == 5 ~ "Spring",
-                            month == 6 | month == 7 | month == 8 ~ "Summer",
-                            month == 9 | month == 10 | month == 11 ~ "Fall")) %>%
-  ggplot(aes(doy, z, colour = season)) +
-  geom_point() +
-  facet_wrap("variable")
+## figure 8 forest cover change plot
+tc_plot <- df_full %>%
+  select(loss, TC2000, grep("SCI_20", colnames(df_full)), grep("INT_20", colnames(df_full)),
+         grep("INTs_20", colnames(df_full)), grep("INTe_20", colnames(df_full))) %>%
+  gather(key = "measurements", value = "value", -loss, -TC2000) %>%
+  mutate(measurements = sub("_.*", "", measurements),
+         tree_c = case_when(TC2000 > 20 & loss == 0 ~ "stable",
+                            TC2000 > 20 & loss == 1 ~ "loss",
+                            TC2000 < 20 ~ "none"),
+         tree_c = factor(as.factor(tree_c), c("stable", "loss", "none"))) %>%
+  filter(complete.cases(tree_c)) %>%
+  ggplot(aes(measurements, value, fill = tree_c)) +
+  geom_boxplot() +
+  scale_fill_brewer(palette = "YlGn", name = "Forest Cover", direction = -1) +
+  theme_bw(base_size = 25, base_family = "Calibri") +
+  theme(panel.grid.major = element_line(linetype = 2, size = .4, color = "dark grey"),
+        panel.grid.minor = element_line(linetype = 2, size = .4, color = "dark grey"),
+        aspect.ratio = 0.6, legend.position = "bottom",
+        legend.text = element_text(size = 20), legend.title = element_text(size = 20))
+tc_plot
+
+## graphic output
+png("../snow_docs/plots/F4_dfo_plot.png", 800, 550, "px")
+dfo_plot
+dev.off()
+
+png("../snow_docs/plots/F5_reg_plot.png", 700, 550, "px")
+reg_plot
+dev.off()
+
+png("../snow_docs/plots/F6_oni_map.png", 800, 600, "px")
+ONI_map
+dev.off()
+
+png("../snow_docs/plots/F7_topo_plot.png", 700, 550, "px")
+topo_plot
+dev.off()
+
+png("../snow_docs/plots/F8_tc_plot.png", 800, 600, "px")
+tc_plot
+dev.off()
